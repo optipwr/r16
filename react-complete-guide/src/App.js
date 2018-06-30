9b48227df26a014ebb128faed5b8d9802b55914e
Michael Irby <michael.irbstoomuch@gmail.com>
2018-06-30 08:52:53 -0400

    new features

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        persons: [
+            { name: 'Michael', age: 32 },
+            { name: 'Joy', age: 31 },
+            { name: 'Anna', age: 30 }
+        ]
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello React</h1>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: vanishing</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            </div>
+        );
+    }
 }
 
 export default App;
