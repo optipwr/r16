f46c5485399257d5381f4bd8ad6615c8a5a90064
Michael Irby <michael.irbstoomuch@gmail.com>
2018-07-13 21:46:52 -0400

    new components

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -4,14 +4,22 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         // console.log('igKey', igKey)
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             // console.log('i', i)
             return <BurgerIngredient key={igKey + i} type={igKey} />;
         });
-    });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+    console.log(transformedIngredients); //the reduce turns the array of 4 arrays into a single array
+    // so that if it is completely empty, we can trigger a message to the user
 
     return (
         <div className={classes.Burger}>
